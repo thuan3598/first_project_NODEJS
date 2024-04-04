d798620cf554511ad207aeefdaa9e671cf55687d
thuan3598 <vhieu5431@gmail.com>
2024-04-04 21:33:34 +0700

    #27 CRUD với Node.js và Sequelize - Sử Dụng ORM Để Sửa Dữ liệu (Edit) - Phần 3

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -28,9 +28,34 @@ let displayGetCRUD=async(req,res)=>{
   console.log(data);
   return res.render('displayCRUD.ejs',{dataTable:data});
 }
+
+let getEditCRUD=async(req,res)=>{
+  let userId =req.query.id;
+  if(userId){
+    let userData=await CRUDService.getUserInfoById(userId);
+
+    return res.render('editCRUD.ejs',{
+      user:userData
+    });
+  }
+  else{
+    return res.send('Users not found');
+  }
+}
+
+let putCRUD=async(req,res)=>{
+  let data=req.body; //Lấy toàn bộ input trong thẻ body
+  let allUsers=await CRUDService.updateUserData(data);
+  return res.render('displayCRUD.ejs',{
+    dataTable:allUsers
+  })
+}
+
 module.exports = {
   getHomePage: getHomePage,
   getCRUD: getCRUD,
   postCRUD: postCRUD,
   displayGetCRUD:displayGetCRUD,
+  getEditCRUD:getEditCRUD,
+  putCRUD:putCRUD
 };
