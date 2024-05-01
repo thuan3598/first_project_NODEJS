6e5ae618d3df7c09cf8033bf4aa5125bbc261369
thuan3598 <vhieu5431@gmail.com>
2024-05-01 15:47:10 +0700

    #52 Import Data Từ EXCEL Và Viết API GET ALLCODE

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,71 +1,83 @@
 import userService from "../services/userService";
 
-let handleLogin=async(req,res)=>{
-    let email=req.body.email;
-    let password=req.body.password;
+let handleLogin = async (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
 
-    if(!email||!password){
-        return res.status(500).json({
-            errCode:1,
-            message:'Missing inputs parameter!'
-        })
-    }
+  if (!email || !password) {
+    return res.status(500).json({
+      errCode: 1,
+      message: "Missing inputs parameter!",
+    });
+  }
 
-    let userData=await userService.handleUserLogin(email,password);
+  let userData = await userService.handleUserLogin(email, password);
 
-    return res.status(200).json({
-       errCode:userData.errCode,
-       message:userData.errMessage,
-       user:userData.user?userData.user:{}
-    })
+  return res.status(200).json({
+    errCode: userData.errCode,
+    message: userData.errMessage,
+    user: userData.user ? userData.user : {},
+  });
 };
 
-let handleGetAllUsers=async(req,res)=>{
-    let id=req.query.id;//All or id
-    let users=await userService.getAllUsers(id);
-    if(!id){
-        return res.status(200).json({
-            errCode:1,
-            message:'Missing requrired parameters',
-            users
-         })
-    }
+let handleGetAllUsers = async (req, res) => {
+  let id = req.query.id; //All or id
+  let users = await userService.getAllUsers(id);
+  if (!id) {
     return res.status(200).json({
-        errCode:0,
-        message:'OK',
-        users
-     })
-}
+      errCode: 1,
+      message: "Missing requrired parameters",
+      users,
+    });
+  }
+  return res.status(200).json({
+    errCode: 0,
+    message: "OK",
+    users,
+  });
+};
 
-let handleCreateNewUser=async(req,res)=>{
-    let message=await userService.createNewUser(req.body);
-    // console.log(message);
-    return res.status(200).json(message);
-}
+let handleCreateNewUser = async (req, res) => {
+  let message = await userService.createNewUser(req.body);
+  // console.log(message);
+  return res.status(200).json(message);
+};
 
-let handleDeleteUser=async(req,res)=>{
+let handleDeleteUser = async (req, res) => {
+  if (!req.body.id) {
+    return res.status(200).json({
+      errCode: 1,
+      errMessage: "Missing required parameter",
+    });
+  }
+  let message = await userService.deleteUser(req.body.id);
+  return res.status(200).json(message);
+};
 
-    if(!req.body.id){
-        return res.status(200).json({
-            errCode:1,
-            errMessage:'Missing required parameter'
-        })
-    }
-    let message=await userService.deleteUser(req.body.id);
-    return res.status(200).json(message);
-}
+let handleEditUser = async (req, res) => {
+  let data = req.body;
+  let message = await userService.updateUserData(data);
+  return res.status(200).json(message);
+};
 
-let handleEditUser=async(req,res)=>{
-    let data=req.body;
-    let message=await userService.updateUserData(data);
-    return res.status(200).json(message);
-}
+let getAllCode = async (req, res) => {
+  try {
+    let data = await userService.getAllCodeService(req.query.type);
+    return res.status(200).json(data);
+  } catch (e) {
+    console.log("Get all code error: ", e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from Server",
+    });
+  }
+};
 
-
-module.exports ={
-    handleLogin :handleLogin,
-    handleGetAllUsers:handleGetAllUsers,
-    handleCreateNewUser:handleCreateNewUser,
-    handleEditUser:handleEditUser,
-    handleDeleteUser:handleDeleteUser
-}
+module.exports = {
+  handleLogin: handleLogin,
+  handleGetAllUsers: handleGetAllUsers,
+  handleCreateNewUser: handleCreateNewUser,
+  handleEditUser: handleEditUser,
+  handleDeleteUser: handleDeleteUser,
+  getAllCode: getAllCode,
+};
